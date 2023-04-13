4edb7e7f1c62cfdd6bfd4bf2df91f4862eb4163e
samibouzid <sami.bouzid@edu.escp.eu>
2023-04-13 07:52:31 +0200

    13/04

--- js/allArticles.js
+++ js/allArticles.js
@@ -1,31 +1,88 @@
 // Your custom JavaScript code for filtering and initializing the DataTable will go here
 $(document).ready(function() {
+    // Define the date format
+    $.fn.dataTable.moment('DD/MM/YYYY');
     // Initialize DataTable
     var table = $('#myTable').DataTable({
-        pageLength: 10, // Show 10 rows per page
-        order: [[0, 'desc']], // Sort by date (most recent first)
-        dom: 'lrtip', // Exclude the search input ("f") from the DataTable layout
+        pageLength: 5, // Show 5 rows per page
+        lengthChange: false,
+        ordering: true,
+        order: [[0, 'desc']], // Sort by date (oldest first, i.e., in ascending order)
+        dom: 'lrtip', // Exclude the search input ("f") from the DataTable layout    
+
         initComplete: function() {
             // Add the category filter dropdown
             this.api().columns(2).every(function() {
                 var column = this;
-                var select = $('<select class="form-control"><option value="">All</option></select>')
-                    .appendTo($("#myTable_wrapper .dataTables_length"))
-                    .on('change', function() {
-                        var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                        column.search(val ? '^' + val + '$' : '', true, false).draw();
-                    });
-
+                var dropdownMenu = $("#categoryDropdownMenu");
+    
                 column.data().unique().sort().each(function(d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
+                    var checkboxId = 'categoryCheckbox' + j;
+                    var checkbox = $('<input type="checkbox" id="' + checkboxId + '" data-category="' + d + '">');
+                    var label = $('<label for="' + checkboxId + '">' + d + '</label>');
+    
+                    checkbox.appendTo(label);
+                    label.appendTo(dropdownMenu);
+    
+                    checkbox.on('change', function() {
+                        var val = $.fn.dataTable.util.escapeRegex($(this).attr('data-category'));
+                        var isChecked = $(this).is(':checked');
+                        if (isChecked) {
+                            column.search(column.search() + '|' + '^' + val + '$', true, false).draw();
+                        } else {
+                            column.search(column.search().replace('|^' + val + '$', ''), true, false).draw();
+                        }
+                    });
                 });
             });
         }
     });
-        // Add event listener to the search button
-        $('.search-button').on('click', function() {
-            searchInDatabase();
-        });
+// Add event listener to the search button
+$('.search-button').on('click', function() {
+    searchInDatabase();
+});
+
+// Variable to store the current sorting direction
+let currentSortOrder = "asc";
+
+// Add event listener for the sort-by-date button
+$("#sort-by-date").on("click", function() {
+    table.order([0, currentSortOrder]).draw();
+
+    if (currentSortOrder === "desc") {
+        currentSortOrder = "asc";
+    } else {
+        currentSortOrder = "desc";
+    }
+});
+
+// Add event listener to the "Filter by Category" button
+$('#filter-by-category').on('click', function() {
+    // Toggle the display of the category dropdown
+    $('.custom-dropdown-menu').toggle();
+    // Remove any search queries when hiding the dropdown
+    if ($('.custom-dropdown-menu').is(':hidden')) {
+        table.column(2).search('').draw();
+    }
+});
+
+// Add event listener for the "Apply" button
+$('#apply-category-filter').on('click', function() {
+  // Get all the checked categories
+  var checkedCategories = [];
+  $('.category-checkbox:checked').each(function() {
+    checkedCategories.push($(this).attr('data-category'));
+  });
+  
+  // Apply the filters to the table
+  var table = $('#myTable').DataTable();
+  table.column(2).search(checkedCategories.join('|'), true, false).draw();
+
+  // Hide the category dropdown
+  $('.custom-dropdown-menu').hide();
+});
+
+
 });
 
 function searchInDatabase() {
